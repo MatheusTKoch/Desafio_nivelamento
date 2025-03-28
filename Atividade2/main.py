7059b0aa30c9cbf143bf67276d6f8e0176d5395f
Matheus <matheustrilhakoch@hotmail.com>
2025-03-27 22:40:42 -0300

    Melhorias na atv2 e no servidor da atv4

--- Atividade2/main.py
+++ Atividade2/main.py
@@ -27,26 +27,35 @@ print("Download Finalizado")
 #Extraindo dados e convertendo para csv
 time.sleep(2)
 try:
-    tables = camelot.read_pdf('Anexo I.pdf', pages='3-181', table_areas=['0,1000,1000,0'], split_text=True, line_scale=60)
+    tables = camelot.read_pdf(
+        'Anexo I.pdf', 
+        pages='3-181',
+        table_areas=['0,1000,1000,0'], 
+        split_text=True, 
+        line_scale=60
+    )
 
     combined_df = pd.concat([table.df for table in tables])
     combined_df = combined_df[~combined_df.apply(lambda row: row.astype(str).str.strip().eq('').all(), axis=1)]
     combined_df = combined_df.reset_index(drop=True)
     
     columns = [
-        'PROCEDIMENTO', 'RN(alteração)', 'VIGÊNCIA', 'OD', 'AMB', 'HCO', 
+        '', 'PROCEDIMENTO', 'RN(alteração)', 'VIGÊNCIA', 'OD', 'AMB', 'HCO', 
         'HSO', 'REF', 'PAC', 'DUT', 'SUBGRUPO', 'GRUPO', 'CAPÍTULO'
     ]
     
     if len(combined_df.columns) > len(columns):
-        combined_df = combined_df.iloc[:, 1:len(columns)+1]
+        combined_df = combined_df.iloc[:, :len(columns)]
     
-    combined_df.columns = columns
+    combined_df.columns = columns[:len(combined_df.columns)]
     
     combined_df.to_csv('Anexo I.csv', index=False, encoding='utf-8-sig', sep=';')
     
     print(f'Arquivo CSV salvo com sucesso. Total de linhas: {len(combined_df)}')
+    print(f'Número de colunas: {len(combined_df.columns)}')
 except Exception as e:
     print(f"Erro ao processar o PDF: {str(e)}")
+    print(f"Número de colunas na tabela: {len(combined_df.columns)}")
+    print("Colunas da tabela:")
 
 
